89452d6db15a6c6f014aad9193c46a104b2cdc11
avivibearchan <chanhungavivi@gmail.com>
2023-08-14 19:06:06 +0800

    Add: model Order & OrderDetail ORM completed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\OrderDetail;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'total_price',
+        'total_discount',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
 }
--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Discount;
+use App\Models\Product;
+use App\Models\Order;
 
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'qty',
+        'unit_price',
+        'order_id',
+        'product_id',
+        'discount_id',
+    ];
+
+    protected $casts = [
+        'qty' => 'integer',
+        'unit_price' => 'integer',
+        'order_id' => 'integer',
+        'product_id' => 'integer',
+        'discount_id' => 'integer'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function discount()
+    {
+        return $this->belongsTo(Discount::class);
+    }
 }
