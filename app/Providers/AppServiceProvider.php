1e20693041ac8a0ae9ab1e916b2af62ced484fd7
avivibearchan <chanhungavivi@gmail.com>
2023-08-14 19:39:07 +0800

    Add: macro response for api success and fail response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,63 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $whitelist = array(
+            '127.0.0.1',
+            '::1'
+        );
+
+        if (!in_array(request()->ip(), $whitelist)) {
+            // URL::forceScheme('https');
+        }
+
+        Response::macro('error', function ($error, $httpStatusCode = 400) {
+
+            $version = 'v1';
+
+            $output = new \Symfony\Component\Console\Output\ConsoleOutput();
+            $output->writeln($httpStatusCode);
+
+            $customFormat = [
+                "server_time" => time(),
+                'version' => $version,
+                'success' => false,
+                'message' => $error,
+                'data' => null
+            ];
+
+            return Response::make($customFormat, $httpStatusCode);
+        });
+
+        Response::macro('api', function ($data, $success = true, $error = null) {
+
+            $version = 'v1';
+
+            if (!is_array($data) && method_exists($data, 'toArray')) {
+                $data = $data->toArray();
+            }
+
+            if (is_array($data) && array_key_exists('data', $data) == true) {
+
+                $customFormat = [
+                    "server_time" => time(),
+                    'version' => $version,
+                    'success' => $success,
+                    'message' => $error
+                ];
+
+                $customFormat = array_merge($customFormat, $data);
+            } else {
+
+                $customFormat = [
+                    "server_time" => time(),
+                    'version' => $version,
+                    'success' => $success,
+                    'message' => $error,
+                    'data' => $data
+                ];
+            }
+
+            return Response::make($customFormat);
+        });
     }
 }
