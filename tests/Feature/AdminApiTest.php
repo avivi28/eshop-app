3e6c540b018fac9739924a04217dc88c19a71762
avivibearchan <chanhungavivi@gmail.com>
2023-08-15 15:15:35 +0800

    Add: adminApiTest for testing create & delete product

--- tests/Feature/AdminApiTest.php
+++ tests/Feature/AdminApiTest.php
@@ -3,14 +3,66 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class AdminApiTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
+
     /**
-     * A basic unit test example.
+     * Test creating a product via API.
+     *
+     * @return void
      */
-    public function test_example(): void
+    public function testCreateProduct()
     {
-        $this->assertTrue(true);
+        // Create a user with role admin
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        // Get user token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $data = [
+            'name' => $this->faker->word,
+            'stock' => $this->faker->randomNumber(2),
+            'price' => $this->faker->randomNumber(2),
+        ];
+
+        $response = $this->postJson('/api/v1/admin/products', $data, ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        //$data[price] need to be multiplied by 100 to be stored in the database
+        $data['price'] = $data['price'] * 100;
+        $this->assertDatabaseHas('products', $data);
+    }
+
+    /**
+     * Test removing a product via API.
+     *
+     * @return void
+     */
+    public function testRemoveProduct()
+    {
+        // Create a user with role admin
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        // Get user token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $product = Product::factory()->create();
+
+        $response = $this->deleteJson("/api/v1/admin/products/{$product->id}", [], ['Authorization' => 'Bearer ' . $token]);
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('products', ['id' => $product->id]);
     }
 }
